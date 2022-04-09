045f9b1982b6cdd34a28c7e6b56d212e4077a44b
Aaron Flores Pasos <aaron.flores7908@alumnos.udg.mx>
2022-04-09 10:59:01 -0500

    Agregar archivos de proyecto.

---
+++ DlgLogin.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Biblioteca_de_Peliculas
+{
+    partial class DlgLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DlgLogin));
+            this.PnlIzquierdo = new System.Windows.Forms.Panel();
+            this.PtbIzquierdo = new System.Windows.Forms.PictureBox();
+            this.TxtUsuario = new System.Windows.Forms.TextBox();
+            this.LblUsuario = new System.Windows.Forms.Label();
+            this.TxtContraseña = new System.Windows.Forms.TextBox();
+            this.LblContraseña = new System.Windows.Forms.Label();
+            this.LblTitulo = new System.Windows.Forms.Label();
+            this.BtnEntrar = new System.Windows.Forms.Button();
+            this.LnblContraseña = new System.Windows.Forms.LinkLabel();
+            this.PtbMinimizar = new System.Windows.Forms.PictureBox();
+            this.PtbCerrar = new System.Windows.Forms.PictureBox();
+            this.CkbMostrarContraseña = new System.Windows.Forms.CheckBox();
+            this.PnlIzquierdo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PtbIzquierdo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PtbMinimizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PtbCerrar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PnlIzquierdo
+            // 
+            this.PnlIzquierdo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.PnlIzquierdo.Controls.Add(this.PtbIzquierdo);
+            this.PnlIzquierdo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PnlIzquierdo.Location = new System.Drawing.Point(0, 0);
+            this.PnlIzquierdo.Name = "PnlIzquierdo";
+            this.PnlIzquierdo.Size = new System.Drawing.Size(250, 330);
+            this.PnlIzquierdo.TabIndex = 0;
+            // 
+            // PtbIzquierdo
+            // 
+            this.PtbIzquierdo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PtbIzquierdo.Image = global::Biblioteca_de_Peliculas.Properties.Resources.Montaña_Nevada;
+            this.PtbIzquierdo.Location = new System.Drawing.Point(0, 0);
+            this.PtbIzquierdo.Name = "PtbIzquierdo";
+            this.PtbIzquierdo.Size = new System.Drawing.Size(250, 330);
+            this.PtbIzquierdo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PtbIzquierdo.TabIndex = 11;
+            this.PtbIzquierdo.TabStop = false;
+            // 
+            // TxtUsuario
+            // 
+            this.TxtUsuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(0)))), ((int)(((byte)(82)))));
+            this.TxtUsuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TxtUsuario.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtUsuario.ForeColor = System.Drawing.Color.DimGray;
+            this.TxtUsuario.Location = new System.Drawing.Point(292, 77);
+            this.TxtUsuario.Name = "TxtUsuario";
+            this.TxtUsuario.Size = new System.Drawing.Size(437, 20);
+            this.TxtUsuario.TabIndex = 1;
+            this.TxtUsuario.Text = "USUARIO";
+            this.TxtUsuario.Enter += new System.EventHandler(this.TxtUsuario_Enter);
+            this.TxtUsuario.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtUsuario_KeyPress);
+            this.TxtUsuario.Leave += new System.EventHandler(this.TxtUsuario_Leave);
+            // 
+            // LblUsuario
+            // 
+            this.LblUsuario.AutoSize = true;
+            this.LblUsuario.ForeColor = System.Drawing.Color.DimGray;
+            this.LblUsuario.Location = new System.Drawing.Point(289, 81);
+            this.LblUsuario.Name = "LblUsuario";
+            this.LblUsuario.Size = new System.Drawing.Size(448, 17);
+            this.LblUsuario.TabIndex = 3;
+            this.LblUsuario.Text = "_______________________________________________________\r\n";
+            // 
+            // TxtContraseña
+            // 
+            this.TxtContraseña.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(0)))), ((int)(((byte)(82)))));
+            this.TxtContraseña.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TxtContraseña.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtContraseña.ForeColor = System.Drawing.Color.DimGray;
+            this.TxtContraseña.Location = new System.Drawing.Point(292, 127);
+            this.TxtContraseña.Name = "TxtContraseña";
+            this.TxtContraseña.Size = new System.Drawing.Size(437, 20);
+            this.TxtContraseña.TabIndex = 2;
+            this.TxtContraseña.Text = "CONTRASEÑA";
+            this.TxtContraseña.Enter += new System.EventHandler(this.TxtContraseña_Enter);
+            this.TxtContraseña.Leave += new System.EventHandler(this.TxtContraseña_Leave);
+            // 
+            // LblContraseña
+            // 
+            this.LblContraseña.AutoSize = true;
+            this.LblContraseña.ForeColor = System.Drawing.Color.DimGray;
+            this.LblContraseña.Location = new System.Drawing.Point(289, 131);
+            this.LblContraseña.Name = "LblContraseña";
+            this.LblContraseña.Size = new System.Drawing.Size(448, 17);
+            this.LblContraseña.TabIndex = 5;
+            this.LblContraseña.Text = "_______________________________________________________\r\n";
+            // 
+            // LblTitulo
+            // 
+            this.LblTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.LblTitulo.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTitulo.ForeColor = System.Drawing.Color.DimGray;
+            this.LblTitulo.Location = new System.Drawing.Point(250, 0);
+            this.LblTitulo.Name = "LblTitulo";
+            this.LblTitulo.Size = new System.Drawing.Size(530, 33);
+            this.LblTitulo.TabIndex = 6;
+            this.LblTitulo.Text = "LOGIN";
+            this.LblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BtnEntrar
+            // 
+            this.BtnEntrar.BackColor = System.Drawing.Color.DimGray;
+            this.BtnEntrar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(0)))), ((int)(((byte)(82)))));
+            this.BtnEntrar.FlatAppearance.BorderSize = 0;
+            this.BtnEntrar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(0)))), ((int)(((byte)(82)))));
+            this.BtnEntrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.BtnEntrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnEntrar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnEntrar.ForeColor = System.Drawing.Color.White;
+            this.BtnEntrar.Location = new System.Drawing.Point(292, 204);
+            this.BtnEntrar.Name = "BtnEntrar";
+            this.BtnEntrar.Size = new System.Drawing.Size(445, 40);
+            this.BtnEntrar.TabIndex = 3;
+            this.BtnEntrar.Text = "ENTRAR";
+            this.BtnEntrar.UseVisualStyleBackColor = false;
+            this.BtnEntrar.Click += new System.EventHandler(this.BtnEntrar_Click);
+            // 
+            // LnblContraseña
+            // 
+            this.LnblContraseña.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.LnblContraseña.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.LnblContraseña.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LnblContraseña.LinkColor = System.Drawing.Color.DimGray;
+            this.LnblContraseña.Location = new System.Drawing.Point(289, 254);
+            this.LnblContraseña.Name = "LnblContraseña";
+            this.LnblContraseña.Size = new System.Drawing.Size(448, 25);
+            this.LnblContraseña.TabIndex = 0;
+            this.LnblContraseña.TabStop = true;
+            this.LnblContraseña.Text = "¿Ha olvidado su contraseña?";
+            this.LnblContraseña.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // PtbMinimizar
+            // 
+            this.PtbMinimizar.Image = global::Biblioteca_de_Peliculas.Properties.Resources.minimizar_signo;
+            this.PtbMinimizar.Location = new System.Drawing.Point(732, 0);
+            this.PtbMinimizar.Name = "PtbMinimizar";
+            this.PtbMinimizar.Size = new System.Drawing.Size(20, 20);
+            this.PtbMinimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PtbMinimizar.TabIndex = 10;
+            this.PtbMinimizar.TabStop = false;
+            this.PtbMinimizar.Click += new System.EventHandler(this.PtbMinimizar_Click);
+            // 
+            // PtbCerrar
+            // 
+            this.PtbCerrar.Image = global::Biblioteca_de_Peliculas.Properties.Resources.poder;
+            this.PtbCerrar.Location = new System.Drawing.Point(758, 0);
+            this.PtbCerrar.Name = "PtbCerrar";
+            this.PtbCerrar.Size = new System.Drawing.Size(20, 20);
+            this.PtbCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PtbCerrar.TabIndex = 9;
+            this.PtbCerrar.TabStop = false;
+            this.PtbCerrar.Click += new System.EventHandler(this.PtbCerrar_Click);
+            // 
+            // CkbMostrarContraseña
+            // 
+            this.CkbMostrarContraseña.AutoSize = true;
+            this.CkbMostrarContraseña.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(0)))), ((int)(((byte)(82)))));
+            this.CkbMostrarContraseña.FlatAppearance.BorderSize = 0;
+            this.CkbMostrarContraseña.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.CkbMostrarContraseña.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CkbMostrarContraseña.Location = new System.Drawing.Point(292, 158);
+            this.CkbMostrarContraseña.Name = "CkbMostrarContraseña";
+            this.CkbMostrarContraseña.Size = new System.Drawing.Size(157, 22);
+            this.CkbMostrarContraseña.TabIndex = 11;
+            this.CkbMostrarContraseña.Text = "Mostrar contraseña";
+            this.CkbMostrarContraseña.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.CkbMostrarContraseña.UseVisualStyleBackColor = true;
+            this.CkbMostrarContraseña.CheckedChanged += new System.EventHandler(this.CkbMostrarContraseña_CheckedChanged);
+            // 
+            // DlgLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(0)))), ((int)(((byte)(82)))));
+            this.ClientSize = new System.Drawing.Size(780, 330);
+            this.Controls.Add(this.CkbMostrarContraseña);
+            this.Controls.Add(this.PtbMinimizar);
+            this.Controls.Add(this.PtbCerrar);
+            this.Controls.Add(this.LnblContraseña);
+            this.Controls.Add(this.BtnEntrar);
+            this.Controls.Add(this.LblTitulo);
+            this.Controls.Add(this.TxtContraseña);
+            this.Controls.Add(this.LblContraseña);
+            this.Controls.Add(this.TxtUsuario);
+            this.Controls.Add(this.PnlIzquierdo);
+            this.Controls.Add(this.LblUsuario);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "DlgLogin";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.DlgLogin_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DlgLogin_MouseDown);
+            this.PnlIzquierdo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PtbIzquierdo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PtbMinimizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PtbCerrar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnlIzquierdo;
+        private System.Windows.Forms.TextBox TxtUsuario;
+        private System.Windows.Forms.Label LblUsuario;
+        private System.Windows.Forms.TextBox TxtContraseña;
+        private System.Windows.Forms.Label LblContraseña;
+        private System.Windows.Forms.Label LblTitulo;
+        private System.Windows.Forms.Button BtnEntrar;
+        private System.Windows.Forms.LinkLabel LnblContraseña;
+        private System.Windows.Forms.PictureBox PtbCerrar;
+        private System.Windows.Forms.PictureBox PtbMinimizar;
+        private System.Windows.Forms.PictureBox PtbIzquierdo;
+        private System.Windows.Forms.CheckBox CkbMostrarContraseña;
+    }
+}
+
---
+++ DlgLogin.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Biblioteca_de_Peliculas
+{
+    public partial class DlgLogin : Form
+    {
+        //╔═══════════╗
+        //║ Variables ║
+        //╚═══════════╝
+        string Usuario = "";
+        string Contraseña = "";
+
+        public DlgLogin()
+        {
+            InitializeComponent();
+        }
+
+        //╔═════════════════════════════════╗
+        //║ Mover la posición de la ventana ║
+        //╚═════════════════════════════════╝
+        [DllImport("user32.dll", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapure();
+        [DllImport("user32.dll", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wsmg, int wparam, int lparam);
+        private void DlgLogin_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapure();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        //╔════════════════╗
+        //║ Cerrar ventana ║
+        //╚════════════════╝
+        private void PtbCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //╔═══════════════════╗
+        //║ Minimizar ventana ║
+        //╚═══════════════════╝
+        private void PtbMinimizar_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        //╔══════════════════════════════════╗
+        //║ Inicialización de cajas de texto ║
+        //╚══════════════════════════════════╝
+        private void DlgLogin_Load(object sender, EventArgs e)
+        {
+            TxtUsuario.Text = "USUARIO";
+            TxtUsuario.ForeColor = Color.DimGray;
+            TxtContraseña.Text = "CONTRASEÑA";
+            TxtContraseña.ForeColor = Color.DimGray;
+        }
+
+        //╔══════════════════════════════════════════════════════╗
+        //║ Al entrar a la caja de texto se borra su predefinido ║
+        //╚══════════════════════════════════════════════════════╝
+        private void TxtUsuario_Enter(object sender, EventArgs e)
+        {
+            Usuario = TxtUsuario.Text;
+            if (Usuario.Equals("USUARIO"))
+            {
+                TxtUsuario.Text = "";
+                TxtUsuario.ForeColor = Color.Black;
+            }
+        }
+
+        //╔══════════════════════════════════════════════════════╗
+        //║ Al entrar a la caja de texto se borra su predefinido ║
+        //╚══════════════════════════════════════════════════════╝
+        private void TxtContraseña_Enter(object sender, EventArgs e)
+        {
+            Contraseña = TxtContraseña.Text;
+            if (Contraseña.Equals("CONTRASEÑA"))
+            {
+                TxtContraseña.Text = "";
+                TxtContraseña.ForeColor = Color.Black;
+                TxtContraseña.UseSystemPasswordChar = true;
+            }
+        }
+
+        //╔══════════════════════════════════════════════════════╗
+        //║ Al dejar la caja de texto se hace hacen validaciones ║
+        //╚══════════════════════════════════════════════════════╝
+        private void TxtUsuario_Leave(object sender, EventArgs e)
+        {
+            Usuario = TxtUsuario.Text;
+            if (Usuario.Equals("USUARIO"))
+            {
+                TxtUsuario.Text = "USUARIO";
+                TxtUsuario.ForeColor = Color.DimGray;
+            }
+            else
+            {
+                if (Usuario.Equals(""))
+                {
+                    TxtUsuario.Text = "USUARIO";
+                    TxtUsuario.ForeColor = Color.DimGray;
+                }
+                else
+                {
+                    TxtUsuario.Text = Usuario;
+                    TxtUsuario.ForeColor = Color.Black;
+                }
+            }
+        }
+
+        //╔══════════════════════════════════════════════════════╗
+        //║ Al dejar la caja de texto se hace hacen validaciones ║
+        //╚══════════════════════════════════════════════════════╝
+        private void TxtContraseña_Leave(object sender, EventArgs e)
+        {
+            Contraseña = TxtContraseña.Text;
+            if (Contraseña.Equals("CONTRASEÑA"))
+            {
+                TxtContraseña.Text = "CONTRASEÑA";
+                TxtContraseña.ForeColor = Color.DimGray;
+                TxtContraseña.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                if (Contraseña.Equals(""))
+                {
+                    TxtContraseña.Text = "CONTRASEÑA";
+                    TxtContraseña.ForeColor = Color.DimGray;
+                    TxtContraseña.UseSystemPasswordChar = false;
+                }
+                else
+                {
+                    TxtContraseña.Text = Contraseña;
+                    TxtContraseña.ForeColor = Color.Black;
+                }
+            }
+        }
+
+        //╔════════════════════════════════════════════════╗
+        //║ Validar solo texto en la caja de texto Usuario ║
+        //╚════════════════════════════════════════════════╝
+        private void TxtUsuario_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 64) || (e.KeyChar >= 91 && e.KeyChar <= 96) || (e.KeyChar >= 123 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("El nombre solo debe contener letras", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        //╔═══════════════════════════════════════════╗
+        //║ Muestra la contraseña de la caja de texto ║
+        //╚═══════════════════════════════════════════╝
+        private void CkbMostrarContraseña_CheckedChanged(object sender, EventArgs e)
+        {
+            TxtContraseña.UseSystemPasswordChar = !CkbMostrarContraseña.Checked;
+        }
+
+        //╔════════════════════════════════════════════════════════════╗
+        //║ Función para mandar un mensaje si la contraseña es erronea ║
+        //╚════════════════════════════════════════════════════════════╝
+        private void ContraseñaIncorrecta()
+        {
+            MessageBox.Show("Contraseña incorrecta", "Incorrecto", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        //╔═══════════════════════════════════════════════╗
+        //║ Sí el perfil existe se despliega otra ventana ║
+        //╚═══════════════════════════════════════════════╝
+        private void BtnEntrar_Click(object sender, EventArgs e)
+        {
+            Usuario = TxtUsuario.Text;
+            string UsuarioFinal = Usuario.ToLower();
+            Contraseña = TxtContraseña.Text;
+
+            switch (UsuarioFinal)
+            {
+                case "aaron":
+                    if (Contraseña == "110803")
+                    {
+                        DlgMenu menu = new DlgMenu();
+                        menu.Show();
+                        TxtUsuario.Text = "USUARIO";
+                        TxtUsuario.ForeColor = Color.DimGray;
+                        TxtContraseña.Text = "CONTRASEÑA";
+                        TxtContraseña.ForeColor = Color.DimGray;
+                        TxtContraseña.UseSystemPasswordChar = false;
+                        WindowState = FormWindowState.Minimized;
+                    }
+                    else
+                    {
+                        ContraseñaIncorrecta();
+                    }
+                    break;
+
+                default:
+                    MessageBox.Show("Datos requeridos no introducidos", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+            }
+        }
+
+    }
+}
