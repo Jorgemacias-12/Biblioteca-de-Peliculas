a665e538ef74202ecdd3e38dde3d21d1dbc31957
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-04-18 17:40:50 -0500

    Agregando clase de utileria y recursos
    	Agregando recursos (iconos) para mostrarlos posteriormente
    	en los formularios dependiendo del requerimiento, además
    	añadir tambien una clase de utileria, proximamente todos los
    	archivos fuente estarán dentro de la carpeta src/.

---
+++ src/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_de_Peliculas.src
+{
+    internal class Utils
+    {
+        public Color getColor(string hex)
+        {
+            return ColorTranslator.FromHtml(hex);
+        }
+    }
+}
