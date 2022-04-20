8ec5ae8625b4df27f4d527ec65fe9e095678e7ec
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-04-20 10:57:45 -0500

    Modificando recursos y configuracion de paquetes

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Biblioteca_de_Peliculas.src.Categoria;
 
 namespace Biblioteca_de_Peliculas
 {
